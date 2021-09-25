58cf05175ec44cd5c3cbe69552b933f823433da6
Alex Cosmas <2alexcosmas@gmail.com>
2021-09-25 18:40:36 +0300

    Update team details

--- components/About/TeamDetails.js
+++ components/About/TeamDetails.js
@@ -3,14 +3,30 @@ import Image from 'next/image'
 export default function TeamDetails() {
 
   const data = [
-
     {
-      name: "Rev. David",
+      name: "REV. DAVID W. KIAMA",
+      title: "FOUNDER",
       details: "David is also the founder and overseer of Power of Praise Ministries in Kenya . This Ministry also runs a charity program. Rev. Kiama has been involved in ministry work for over 20 years",
     },
     {
-      name: "Hannah",
-      details: "Lorem is also the founder and overseer of Power of Praise Ministries in Kenya . This Ministry also runs a charity program. Rev. Kiama has been involved in ministry work for over 20 years",
+      name: "RICHARD K. WANJAU",
+      title: "MANAGING DIRECTOR",
+      details: "Richard has been managing several printing and design companies in Nairobi for more than 25 years",
+    },
+    {
+      name: "JANE W. ITEGI",
+      title: "GENERAL SECRETARY",
+      details: "Jane has a lot of experience working as a secretary for various institutions in Kenya",
+    },
+    {
+      name: "GLADYS W. NJERI",
+      title: "PROJECT DIRECTOR",
+      details: "Gladys is a social worker - she has been involved in doing charity work, especially in the Nairobi slums."
+    },
+    {
+      name: "HANNAH N. NGETHE",
+      title: "ACCOUNTANT",
+      details: "Hannah has a great deal of experience in the accounting industry and video development. She currently works with the Kenyan county government."
     }
 
   ];
@@ -22,6 +38,7 @@ export default function TeamDetails() {
             <div key={idx} className='bg-gray-100 '>
               <div className='px-10 py-12'>
                 <h1> {teamdetails.name}</h1>
+                <h3> {teamdetails.title}</h3>
                 <p>{teamdetails.details}</p>
               </div>
             </div>
@@ -32,5 +49,6 @@ export default function TeamDetails() {
   );
 
 }
+
 
 
