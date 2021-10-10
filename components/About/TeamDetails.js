f0782b61e211ba53cbf2ab6ade89002ff2352a0d
Alex Cosmas <2alexcosmas@gmail.com>
2021-10-10 12:31:10 +0300

    Fixes

--- components/About/TeamDetails.js
+++ components/About/TeamDetails.js
@@ -1,54 +1,91 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function TeamDetails() {
-
   const data = [
     {
       name: "REV. DAVID W. KIAMA",
       title: "FOUNDER",
-      details: "David is also the founder and overseer of Power of Praise Ministries in Kenya . This Ministry also runs a charity program. Rev. Kiama has been involved in ministry work for over 20 years",
+      details:
+        "Rev. David Kiama is the CEO and founder of MDF and Power of Praise Ministries. Praise Ministries also runs a charity program which Rev. Kiama has been involved in ministry work for over 20 years",
+      src: {
+        original:
+          "https://res.cloudinary.com/alexoc/image/upload/v1633856626/Projects/Miracle%20Dave%20Foundation/Profile/David_z5yaf3.webp",
+      },
     },
     {
       name: "RICHARD K. WANJAU",
       title: "MANAGING DIRECTOR",
-      details: "Richard has been managing several printing and design companies in Nairobi for more than 25 years",
+      details:
+        "Richard has been managing several printing and design companies in Nairobi for more than 25 years",
+      src: {
+        original:
+          "https://res.cloudinary.com/alexoc/image/upload/v1633856626/Projects/Miracle%20Dave%20Foundation/Profile/Richard_k5ms1i.webp",
+      },
     },
     {
       name: "JANE W. ITEGI",
       title: "GENERAL SECRETARY",
-      details: "Jane has a lot of experience working as a secretary for various institutions in Kenya",
+      details:
+        "Jane has a lot of experience working as a secretary for various institutions in Kenya",
+      src: {
+        original:
+          "https://res.cloudinary.com/alexoc/image/upload/v1633856626/Projects/Miracle%20Dave%20Foundation/Profile/Jane_rhhe5w.webp",
+      },
     },
     {
       name: "GLADYS W. NJERI",
       title: "PROJECT DIRECTOR",
-      details: "Gladys is a social worker - she has been involved in doing charity work, especially in the Nairobi slums."
+      details:
+        "Gladys is a social worker - she has been involved in doing charity work, especially in the Nairobi slums.",
+      src: {
+        original:
+          "https://res.cloudinary.com/alexoc/image/upload/v1633856626/Projects/Miracle%20Dave%20Foundation/Profile/Hannah_vvrp3f.webp",
+      },
     },
     {
       name: "HANNAH N. NGETHE",
       title: "ACCOUNTANT",
-      details: "Hannah has a great deal of experience in the accounting industry and video development. She currently works with the Kenyan county government."
-    }
-
+      details:
+        "Hannah has a great deal of experience in the accounting industry and video development. She currently works with the Kenyan county government.",
+      src: {
+        original:
+          "https://res.cloudinary.com/alexoc/image/upload/v1633856626/Projects/Miracle%20Dave%20Foundation/Profile/Gladys_lhp97x.webp",
+      },
+    },
   ];
   return (
     <>
       <div className="grid grid-cols-2 gap-10 ">
         {data.map(function (teamdetails, idx) {
           return (
-            <div key={idx} className='bg-gray-100 '>
-              <div className='px-10 py-12'>
-                <h1> {teamdetails.name}</h1>
-                <h3> {teamdetails.title}</h3>
-                <p>{teamdetails.details}</p>
+            <div key={idx} className="bg-gray-100">
+              <div className="px-10 py-12">
+                <div className="py-5 border-b-2">
+                  <div className="relative w-24 h-24 ">
+                    <Image
+                      src={teamdetails.src?.original}
+                      alt=""
+                      layout="fill"
+                      objectFit="contain"
+                      className=""
+                    />
+                  </div>
+                </div>
+
+                <div className="flex py-3">
+                  <div>
+                    <h1> {teamdetails.name}</h1>
+                    <h3> {teamdetails.title}</h3>
+                  </div>
+                </div>
+                <div className="pt-2">
+                  <p>{teamdetails.details}</p>
+                </div>
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </>
   );
-
 }
-
-
-
--- components/HowtoHelp/HelpDetails.js
+++ components/HowtoHelp/HelpDetails.js
@@ -1,60 +1,52 @@
 export default function HelpDetails() {
-
   const data = [
-
     {
       name: "Priscilla Hafula",
-      details: "8 Years",
+      details: "9 Years",
     },
     {
       name: "Elyes Ondiso",
-      details: "5 Years",
+      details: "6 Years",
     },
     {
       name: "Pamela Nyandiko",
-      details: "3 Years",
+      details: "4 Years",
     },
     {
       name: "Samuel Mutende Kamau",
-      details: "7 Years",
+      details: "8 Years",
     },
     {
       name: "Reuben Ng’ethe Kamau",
-      details: "13 Years",
+      details: "14 Years",
     },
-
-
   ];
-
 
   return (
     <>
-      <div className='p-6'>
-
-        <h2 className='mdf-h1'>
-          Your <span className='text-optional'>Impact </span>
+      <div className="p-6">
+        <h2 className="mdf-h1">
+          Your <span className="text-optional">Impact </span>
         </h2>
 
-        <p className='py-3'>Below is a list of children under MDF seeking sponsorship managed by our Regional Coordinator </p>
+        <p className="py-3">
+          Below is a list of children under MDF seeking sponsorship managed by
+          our Regional Coordinator{" "}
+        </p>
 
         <div className="grid gap-5 my-10 md:grid-cols-3">
           {data.map(function (teamdetails, idx) {
             return (
-              <div key={idx} className='bg-gray-100 '>
-                <div className='px-10 py-12'>
+              <div key={idx} className="bg-gray-100 ">
+                <div className="px-10 py-12">
                   <h1> {teamdetails.name}</h1>
                   <p>{teamdetails.details}</p>
                 </div>
               </div>
-            )
+            );
           })}
         </div>
-
-
-
-
       </div>
     </>
-  )
+  );
 }
-
