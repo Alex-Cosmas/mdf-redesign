cd35d76b5646e62de56d912ae516d5bd3052af94
Alex Cosmas <cosmasalex365@gmail.com>
2023-08-07 15:14:46 +0300

    Image fix

--- components/HomePage/OurMission.js
+++ components/HomePage/OurMission.js
@@ -1,22 +1,22 @@
-import Image from "next/legacy/image"
+import Image from "next/legacy/image";
 
 export default function OurMission() {
   return (
     <>
-      <section className='grid flex-col-reverse gap-8 pb-10 md:pt-32 md:grid-cols-2'>
-        <div className='flex flex-col justify-end h-full md:-mt-14'>
-          <div className='relative h-96 min-w-[300px]  border-4 border-secondary shadow-md'>
+      <section className="grid flex-col-reverse gap-8 pb-10 md:pt-32 md:grid-cols-2">
+        <div className="flex flex-col justify-end h-full md:-mt-14">
+          <div className="relative h-96 min-w-[300px]  border-4 border-secondary shadow-md">
             <Image
-              src='/mdf assets/Happy_JPG.webp'
-              alt='HeroBg'
-              layout='fill'
-              objectFit='cover'
+              src="/MDFAssets/Happy_JPG.webp"
+              alt="Our Mission"
+              layout="fill"
+              objectFit="cover"
             />
           </div>
         </div>
-        <div className='p-6 space-y-8 '>
-          <h2 className='mdf-h1'>
-            Our <span className='text-optional'>Mission</span>
+        <div className="p-6 space-y-8 ">
+          <h2 className="mdf-h1">
+            Our <span className="text-optional">Mission</span>
           </h2>
           <div>
             <p>We endeavor to help children in need, one at a time.</p>
@@ -29,5 +29,5 @@ export default function OurMission() {
         </div>
       </section>
     </>
-  )
+  );
 }
